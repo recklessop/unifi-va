f35ada13500cfee174d491e52056bc11fae97ace
Justin Paul <justin@jpaul.me>
2019-02-24 10:07:36 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 sudo apt update
+sudo apt install python3-pip
+sudo pip3 install -r /home/unifi/unifi-va/requirements.txt
 sudo apt install curl net-tools apt-transport-https openjdk-8-jre-headless -y
 sudo apt install mongodb-org-server mongodb-org-shell mongodb-org-tools -y
 sudo apt install binutils ca-certificates-java java-common -y
