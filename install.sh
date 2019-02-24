c654a27a0f51ae93991c28df088ced8978c4b8e9
Justin Paul <justin@jpaul.me>
2019-02-24 11:05:21 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ dirpath=$(pwd)
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 
-echo "unifi ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/apt/sources.list.d/mongodb-org-3.4.list
+echo "unifi ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoer
 
 cat > /home/$me/override.conf <<EOF
 [Service]
