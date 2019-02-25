77cc7f267f5bde4732b75c26338751667967e1d7
Justin Paul <justin@jpaul.me>
2019-02-24 22:54:19 -0500

    Update config-vm.ps1
    moving dd to a build task

--- build/config-vm.ps1
+++ build/config-vm.ps1
@@ -3,4 +3,3 @@ Write-Output $vm_ip
 
 ssh -i .ssh\id_rsa.pub unifi@$vm_ip "sudo apt update; sudo apt upgrade -y"
 ssh -i .ssh\id_rsa.pub unifi@$vm_ip  "git clone https://github.com/recklessop/unifi-va.git; cd unifi-va; sudo bash /home/unifi/unifi-va/install.sh"
-ssh -i .ssh\id_rsa.pub unifi@$vm_ip  "echo 'Zeroing out virtual disk'; dd if=/dev/zero of=temp.img bs=1M; rm temp.img"
