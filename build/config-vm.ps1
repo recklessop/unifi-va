d56c664151e331002f7581e1c7438a14bee3eeeb
Justin Paul <justin@jpaul.me>
2019-02-25 00:12:15 -0500

    Update config-vm.ps1

--- build/config-vm.ps1
+++ build/config-vm.ps1
@@ -1,5 +1,5 @@
 $vm_ip = $ENV:vm_ip
 Write-Output $vm_ip
 
-ssh -i .ssh\id_rsa.pub unifi@$vm_ip "sudo apt update; sudo apt upgrade -y; sudo apt install pv -y"
+ssh -i .ssh\id_rsa.pub unifi@$vm_ip "sudo apt update; sudo apt upgrade -y;"
 ssh -i .ssh\id_rsa.pub unifi@$vm_ip  "git clone https://github.com/recklessop/unifi-va.git; cd unifi-va; sudo bash /home/unifi/unifi-va/install.sh"
