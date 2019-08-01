e4efa69fbd4c512315b4a4014794af43198c72ca
Justin Paul <justin@jpaul.me>
2019-08-01 13:20:37 -0400

    Update update-apt.sh

--- update-apt.sh
+++ update-apt.sh
@@ -1,4 +1,4 @@
-echo "This script updates your apt sources with the official unifi apt repo and then upgrades all packages on the unifi appliance
+echo "This script updates your apt sources with the official unifi apt repo and then upgrades all packages on the unifi appliance"
 echo "deb http://www.ui.com/downloads/unifi/debian stable ubiquiti" | sudo tee /etc/apt/sources.list.d/100-ubnt.list
 sudo apt-key adv --recv-keys 06E85760C0A52C50
 sudo apt update
